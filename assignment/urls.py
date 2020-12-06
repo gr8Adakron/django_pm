d9d29aadb35cb59db73beb2f66950eda0e3e9501
gr8_adakron <afzal@crowdanalytix.com>
2020-12-06 19:30:03 +0530

    cleaning repo

--- assignment/urls.py
+++ assignment/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('pm/', include('project_manager.urls')),
+    path('', include('project_manager.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
